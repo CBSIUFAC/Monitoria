3229b04a30d1cb5d77293da874adf116b567a9da
josinojoao <josinojoao@gmail.com>
2014-11-24 14:17:19 -0200

    Edição Master Dao

--- monitorias/src/test/java/DAO/MasterDAO.java
+++ monitorias/src/test/java/DAO/MasterDAO.java
@@ -1,7 +1,58 @@
 package DAO;
 
-public class MasterDAO {
+import java.io.Serializable;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import util.HibernateUtil;
 
-	int teste;
+public class MasterDAO {
 	
+	public Session getSession(){
+		return HibernateUtil.getSessionFactory().openSession();
+	}
+	
+	public void inserirObjeto(Object obj){
+		Session s = getSession();
+		s.beginTransaction();
+		s.save(obj);
+		s.getTransaction().commit();
+		s.close();
+	}
+	
+	public void deletarObjeto(Object obj){
+		Session s = getSession();
+		s.beginTransaction();
+		s.delete(obj);
+		s.getTransaction().commit();
+		s.close();
+	}
+	
+	public void atualizarObjeto(Object obj){
+		Session s = getSession();
+		s.beginTransaction();
+		s.update(obj);
+		s.getTransaction().commit();
+		s.close();
+	}
+	
+	//Retorna qualquer objeto
+	public <T extends Serializable> T getObjeto(Class<T> classe, int id){
+		Session s = getSession();
+		s.beginTransaction();
+		Serializable retorno = (Serializable)s.get(classe, id);
+		s.getTransaction().commit();
+		s.close();
+		return (T)retorno;
+	}
+	//Retorna lista de qualquer objeto
+	public <T extends Serializable> List<T> getLista(String str){
+		Session s = getSession();
+		s.beginTransaction();
+		Query qr = s.createQuery(str);
+		List<T> retorno = qr.list();
+		s.getTransaction().commit();
+		s.close();
+		return retorno;
+	}	
 }
