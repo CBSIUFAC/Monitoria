162167d6cc0461b149566eb01c1f5c9bf7c15e1f
josinojoao <josinojoao@gmail.com>
2014-11-23 17:28:26 -0800

    Teste

--- monitorias/src/test/java/entity/Edital.java
+++ monitorias/src/test/java/entity/Edital.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
