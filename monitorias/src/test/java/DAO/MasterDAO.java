442a0b7943f1adc96b6aad691f60209c2d7cf6d7
josinojoao <josinojoao@gmail.com>
2014-11-26 22:28:19 -0800

    Adição da verificação do commit a fim de verificar se a transação realmente ocorreu, resolvendo o problema dos deadlocks, onde transações que ocorriam algum erro não tinham o fechamento da sessão do hibernate chmada, criando assim processo fantasma que travava todos os outros.

--- monitorias/src/test/java/DAO/MasterDAO.java
+++ monitorias/src/test/java/DAO/MasterDAO.java
@@ -2,58 +2,101 @@ package DAO;
 
 import java.io.Serializable;
 import java.util.List;
+
+import javax.transaction.SystemException;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import util.HibernateUtil;
 
 public class MasterDAO {
-	//Teste de clone do projeto
 	
 	public Session getSession(){
 		return HibernateUtil.getSessionFactory().openSession();
 	}
 	
-	public void inserirObjeto(Object obj){
-		Session s = getSession();
-		s.beginTransaction();
-		s.save(obj);
-		s.getTransaction().commit();
-		s.close();
+	/* Foi adicionada uma verificação se a transação ocorreu ou não, para evitar deadlocks. */
+	
+	public void inserirObjeto(Object obj) {
+		Session s = null;
+		Transaction tx = null; 
+		try {
+			s = getSession();
+			tx = s.beginTransaction();
+			s.save(obj);
+			tx.commit();
+		} catch (Exception e) {
+	        if (tx != null) tx.rollback();
+	    } finally {
+	        if (s != null) s.close();
+	    }
 	}
 	
-	public void deletarObjeto(Object obj){
-		Session s = getSession();
-		s.beginTransaction();
-		s.delete(obj);
-		s.getTransaction().commit();
-		s.close();
+	public void deletarObjeto(Object obj) {
+		Session s = null;
+		Transaction tx = null; 
+		try {
+			s = getSession();
+			tx = s.beginTransaction();
+			s.delete(obj);
+			tx.commit();
+		} catch (Exception e) {
+	        if (tx != null) tx.rollback();
+	    } finally {
+	        if (s != null) s.close();
+	    }
 	}
 	
-	public void atualizarObjeto(Object obj){
-		Session s = getSession();
-		s.beginTransaction();
-		s.update(obj);
-		s.getTransaction().commit();
-		s.close();
+	public void atualizarObjeto(Object obj) {
+		Session s = null;
+		Transaction tx = null; 
+		try {
+			s = getSession();
+			tx = s.beginTransaction();
+			s.update(obj);
+			tx.commit();
+		} catch (Exception e) {
+	        if (tx != null) tx.rollback();
+	    } finally {
+	        if (s != null) s.close();
+	    }
 	}
 	
 	//Retorna qualquer objeto
 	public <T extends Serializable> T getObjeto(Class<T> classe, int id){
-		Session s = getSession();
-		s.beginTransaction();
-		Serializable retorno = (Serializable)s.get(classe, id);
-		s.getTransaction().commit();
-		s.close();
+		Session s = null;
+		Transaction tx = null;
+		Serializable retorno = null;
+		try {
+			s = getSession();
+			tx = s.beginTransaction();
+			retorno = (Serializable)s.get(classe, id);
+			s.getTransaction().commit();
+		} catch (Exception e) {
+			if(tx != null) tx.rollback();
+		} finally {
+			if(s != null) s.close();
+		}
 		return (T)retorno;
 	}
 	//Retorna lista de qualquer objeto
 	public <T extends Serializable> List<T> getLista(String str){
-		Session s = getSession();
-		s.beginTransaction();
+		Session s = null;
+		Transaction tx = null;
+		List<T> retorno = null;
+		try {
+		s = getSession();
+		tx = s.beginTransaction();
 		Query qr = s.createQuery(str);
-		List<T> retorno = qr.list();
+		retorno = qr.list();
 		s.getTransaction().commit();
-		s.close();
+		} catch (Exception e) {
+			if(tx!=null) tx.rollback();
+		} finally {
+			if( s!=null) s.close();
+		}
 		return retorno;
 	}	
 }
